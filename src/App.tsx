72d5cd5f70042caa5c256134c643c86e3f55f954
sjuhan123 <sjuhan123@gmail.com>
2023-08-04 22:49:50 +0900

    🔧 chore: 초기 defaultTheme 및 ThemeProvider 세팅 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import ToDo from './pages/ToDo';
 import Welcome from './pages/Welcome';
+import { theme } from './styles/theme';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Welcome />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/todo" element={<ToDo />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/todo" element={<ToDo />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 };
 
---
+++ src/styles/theme.ts
@@ -0,0 +1,11 @@
+import { DefaultTheme } from 'styled-components';
+
+const theme: DefaultTheme = {
+  COLORS: {
+    TURQUISE: 'rgb(36, 224, 188)',
+    ROYAL_BULE: 'rgb(67, 139, 255)',
+    DEEP_BLUE: 'rgb(44, 91, 242)',
+  },
+};
+
+export { theme };
