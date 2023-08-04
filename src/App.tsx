fa9b202793d026b083ecbcdad282865595d21ef3
sjuhan123 <sjuhan123@gmail.com>
2023-08-04 21:24:16 +0900

    ✨ feat: 라우터로 path 설정 및 컴포넌트 초기 구현 - Welcome 컴포넌트 초기 구현 - SignIn 컴포넌트 초기 구현 - SignUp 컴포넌트 초기 구현 - ToDo 컴포넌트 초기 구현 - App 컴포넌트에 라우터로 path 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import ToDo from './pages/ToDo';
+import Welcome from './pages/Welcome';
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/todo" element={<ToDo />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
