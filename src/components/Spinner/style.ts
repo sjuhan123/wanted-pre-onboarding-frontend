c1d9a20dbd1f3a2d17d1fdca466a2f6b7ffa34a5
sjuhan123 <sjuhan123@gmail.com>
2023-08-08 23:05:40 +0900

    ✨ feat: Spinner 컴포넌트 구현

---
+++ src/components/Spinner/style.ts
@@ -0,0 +1,24 @@
+import styled, { keyframes } from 'styled-components';
+
+export const spinAnimation = keyframes`
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+`;
+
+export const Spinner = styled.div`
+  display: flex;
+  box-sizing: border-box;
+  width: 13px;
+  height: 13px;
+  border: 3px solid ${({ theme }) => theme.COLORS.WHITE};
+  border-top-color: transparent;
+  border-radius: 50%;
+  animation: ${spinAnimation} 1s linear infinite;
+`;
+
+/* ${({ isDynamic }) =>
+    isDynamic &&
+    css`
+      top: 15%;
+      left: 15%;
+    `} */
