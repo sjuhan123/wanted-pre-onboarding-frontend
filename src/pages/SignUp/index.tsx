a94c2bec5dd30a522825de8126cb97a3383a12c9
sjuhan123 <sjuhan123@gmail.com>
2023-08-08 23:13:23 +0900

    ✨ feat: SignUp 컴포넌트 API 연동
        - signup 회원가입 API 연동
        - 회원가입 성공시 로직 구현 완료
        - token 여부에 따라 리다이렉트 로직 구현 완료

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,19 +1,45 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Button from '../../components/Button';
 import Layout from '../../components/Layout';
+import { Spinner } from '../../components/Spinner';
 import TextInput from '../../components/TextInput';
-import { DATA_TEST_ID } from '../../constants';
+import { DATA_TEST_ID, URL } from '../../constants';
+import { END_POINT } from '../../constants/endpoint';
+import useFetch from '../../hooks/useFetch';
 import useForm from '../../hooks/useForm';
 import useValidation from '../../hooks/useValidation';
 
 const SignUp = () => {
+  const navigate = useNavigate();
+  const { status, fetchData } = useFetch();
+
   const { values, handleChange, handleSubmit } = useForm({
     initialValues: { email: '', password: '' },
     onSubmit: values => {
-      console.log(values);
+      fetchData({
+        url: END_POINT.SIGN_UP,
+        method: 'POST',
+        isGetData: false,
+        body: JSON.stringify(values),
+        contentsType: { 'Content-Type': 'application/json' },
+      });
     },
   });
 
   const { isValid, isEmailValid, isPasswordValid } = useValidation(values.email, values.password);
+
+  useEffect(() => {
+    if (status === 'success') {
+      navigate(URL.SIGN_IN);
+    }
+  }, [status]);
+
+  useEffect(() => {
+    if (localStorage.getItem('token')) {
+      navigate(URL.TODO);
+    }
+  }, []);
 
   return (
     <Layout header="SIGN UP">
@@ -37,7 +63,7 @@ const SignUp = () => {
         size="M"
         type="secondary"
         dataTestId={DATA_TEST_ID.BUTTON.SIGN_UP}
-        buttonText="회원가입"
+        buttonContent={status === 'loading' ? <Spinner /> : '회원가입'}
         disabled={!isValid}
         onClick={handleSubmit}
       />
