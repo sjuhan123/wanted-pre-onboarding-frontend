02a8f79f966a4793b6504854738a67bf0d345a17
sjuhan123 <sjuhan123@gmail.com>
2023-08-08 20:13:02 +0900

    ✨ feat: Welcome 컴포넌트 구현

--- src/pages/Welcome/index.tsx
+++ src/pages/Welcome/index.tsx
@@ -1,5 +1,17 @@
+import { useNavigate } from 'react-router-dom';
+import Button from '../../components/Button';
+import Layout from '../../components/Layout';
+import { URL } from '../../constants';
+
 const Welcome = () => {
-  return <div>Welcome</div>;
+  const navigate = useNavigate();
+
+  return (
+    <Layout header="TODO APP">
+      <Button size="M" type="primary" onClick={() => navigate(URL.SIGN_UP)} buttonText="회원가입" />
+      <Button size="M" type="primary" onClick={() => navigate(URL.SIGN_IN)} buttonText="로그인" />
+    </Layout>
+  );
 };
 
 export default Welcome;
