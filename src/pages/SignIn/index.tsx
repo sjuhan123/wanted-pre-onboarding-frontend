a629754ef637e3bed3688dde4100faf3588c6c7e
sjuhan123 <sjuhan123@gmail.com>
2023-08-08 23:12:33 +0900

    ✨ feat: SignIn 컴포넌트 API 연동
    - signin 회원가입 API 연동
    - 회원가입 성공시 로직 구현 완료
    - token 여부에 따라 리다이렉트 로직 구현 완료

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,19 +1,50 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Button from '../../components/Button';
 import Layout from '../../components/Layout';
+import { Spinner } from '../../components/Spinner/style';
 import TextInput from '../../components/TextInput';
-import { DATA_TEST_ID } from '../../constants';
+import { DATA_TEST_ID, URL } from '../../constants';
+import { END_POINT } from '../../constants/endpoint';
+import useFetch from '../../hooks/useFetch';
 import useForm from '../../hooks/useForm';
 import useValidation from '../../hooks/useValidation';
 
+type AccessToken = {
+  access_token: string;
+};
+
 const SignIn = () => {
+  const navigate = useNavigate();
+  const { data, status, fetchData } = useFetch<AccessToken>();
+
   const { values, handleChange, handleSubmit } = useForm({
     initialValues: { email: '', password: '' },
     onSubmit: values => {
-      console.log(values);
+      fetchData({
+        url: END_POINT.SIGN_IN,
+        method: 'POST',
+        isGetData: true,
+        body: JSON.stringify(values),
+        contentsType: { 'Content-Type': 'application/json' },
+      });
     },
   });
 
   const { isValid, isEmailValid, isPasswordValid } = useValidation(values.email, values.password);
+
+  useEffect(() => {
+    if (data && status === 'success') {
+      localStorage.setItem('token', data.access_token);
+      navigate(URL.TODO);
+    }
+  }, [status, data]);
+
+  useEffect(() => {
+    if (localStorage.getItem('token')) {
+      navigate(URL.TODO);
+    }
+  }, []);
 
   return (
     <Layout header="SIGN IN">
@@ -37,7 +68,7 @@ const SignIn = () => {
         size="M"
         type="secondary"
         dataTestId={DATA_TEST_ID.BUTTON.SIGN_IN}
-        buttonText="로그인"
+        buttonContent={status === 'loading' ? <Spinner /> : '로그인'}
         disabled={!isValid}
         onClick={handleSubmit}
       />
