b4ba46b2710d8e8a5365bdac82690a9b03093251
sjuhan123 <sjuhan123@gmail.com>
2023-08-08 20:13:40 +0900

    ✨ feat: SignIn 컴포넌트 구현
    - 뷰 구현
    - 클라이언트 로직 구현

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,5 +1,48 @@
+import Button from '../../components/Button';
+import Layout from '../../components/Layout';
+import TextInput from '../../components/TextInput';
+import { DATA_TEST_ID } from '../../constants';
+import useForm from '../../hooks/useForm';
+import useValidation from '../../hooks/useValidation';
+
 const SignIn = () => {
-  return <div>SignIn</div>;
+  const { values, handleChange, handleSubmit } = useForm({
+    initialValues: { email: '', password: '' },
+    onSubmit: values => {
+      console.log(values);
+    },
+  });
+
+  const { isValid, isEmailValid, isPasswordValid } = useValidation(values.email, values.password);
+
+  return (
+    <Layout header="SIGN IN">
+      <TextInput
+        name="email"
+        dataTestId={DATA_TEST_ID.INPUT.EMAIL}
+        placeholder="이메일을 입력해주세요."
+        onChange={handleChange}
+        $isValid={!isEmailValid && values.email.length > 0}
+        helperText={'이메일은 "@"이 포함되어야 합니다.'}
+      />
+      <TextInput
+        name="password"
+        dataTestId={DATA_TEST_ID.INPUT.PASSWORD}
+        placeholder="비밀번호를 입력해주세요."
+        onChange={handleChange}
+        $isValid={!isPasswordValid && values.password.length > 0}
+        helperText={'비밀번호는 8자 이상이어야 합니다.'}
+      />
+      <Button
+        size="M"
+        type="secondary"
+        dataTestId={DATA_TEST_ID.BUTTON.SIGN_IN}
+        buttonText="로그인"
+        disabled={!isValid}
+        onClick={handleSubmit}
+      />
+    </Layout>
+  );
 };
 
 export default SignIn;
